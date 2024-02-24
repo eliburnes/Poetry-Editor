62488b0536227ddb2e88bfe55e3436e2bfb1269d
Eli Burnes <eli@lines.by>
2024-02-24 11:46:55 -0500

    create appkit project

---
+++ Poetry Editor/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Poetry Editor
+//
+//  Created by Eli Burnes on 2/24/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
